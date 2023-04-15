3e698a9d819030bbc579c39daf65e44bd08ea40e
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-04-15 21:17:30 +0300

    Add filter(is_on_main=True)

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,7 +5,9 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title')
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+        ).filter(is_on_main=True)
     context = {
         'ice_cream_list': ice_cream_list,
     }
