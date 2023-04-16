80a5c1816804271e7770cdfbfb7df72451fea7ce
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-04-16 12:56:33 +0300

    Test ORDER BY in Category

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,14 +1,26 @@
+# from django.db.models import Q
 from django.shortcuts import render
 
-from ice_cream.models import IceCream
+from ice_cream.models import Category, IceCream
 
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values(
-        'id', 'title', 'description'
-        ).filter(is_on_main=True)
+    # ice_cream_list = IceCream.objects.values(
+    #     'id', 'title', 'description'
+    # ).filter(
+    #     Q(is_on_main=True)
+    #     | (Q(title__contains='пломбир') & Q(is_published=True))
+    # )
+    categories = Category.objects.values(
+        'id', 'output_order', 'title'
+    ).order_by(
+        'output_order', 'title'
+    )
+    # context = {
+    #     'ice_cream_list': ice_cream_list,
+    # }
     context = {
-        'ice_cream_list': ice_cream_list,
+        'categories': categories
     }
     return render(request, template_name, context)
