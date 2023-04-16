91f7bdb8409f26683970269fab3d70b8e4e78c56
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-04-16 15:46:10 +0300

    Test JOIN with .select_related()

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -5,7 +5,7 @@ from ice_cream.models import IceCream
 
 def index(request):
     template_name = 'homepage/index.html'
-    ice_cream_list = IceCream.objects.values('id', 'title', 'category__title')
+    ice_cream_list = IceCream.objects.select_related('category')
     context = {
         'ice_cream_list': ice_cream_list,
     }
