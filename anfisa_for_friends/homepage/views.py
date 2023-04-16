e60e4b840217f12744b38230346eaac66b44005a
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-04-16 14:27:08 +0300

    Add get_object_or_404

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,26 +1,16 @@
-# from django.db.models import Q
 from django.shortcuts import render
 
-from ice_cream.models import Category, IceCream
+from ice_cream.models import IceCream
 
 
 def index(request):
     template_name = 'homepage/index.html'
-    # ice_cream_list = IceCream.objects.values(
-    #     'id', 'title', 'description'
-    # ).filter(
-    #     Q(is_on_main=True)
-    #     | (Q(title__contains='пломбир') & Q(is_published=True))
-    # )
-    categories = Category.objects.values(
-        'id', 'output_order', 'title'
-    ).order_by(
-        'output_order', 'title'
-    )
-    # context = {
-    #     'ice_cream_list': ice_cream_list,
-    # }
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description'
+    ).filter(
+        is_published=True, is_on_main=True
+    ).order_by('title')[1:4]
     context = {
-        'categories': categories
+        'ice_cream_list': ice_cream_list,
     }
     return render(request, template_name, context)
