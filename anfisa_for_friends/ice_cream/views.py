f840ee9568e8de524832ad94b75609bcb0ede3a7
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-04-16 15:00:00 +0300

    Test get_object_or_404 with .objects.values() and .filters()

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -6,7 +6,9 @@ from ice_cream.models import IceCream
 def ice_cream_detail(request, pk):
     template_name = 'ice_cream/detail.html'
     ice_cream = get_object_or_404(
-        IceCream.objects.values('title', 'description'),
+        IceCream.objects.values(
+            'title', 'description'
+        ).filter(is_published=True),
         pk=pk
     )
     context = {
